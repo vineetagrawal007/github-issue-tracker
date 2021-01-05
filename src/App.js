3658268817534953bb8cf416e55298cdda0c802c
Vineet Kumar Agrawal <vineet.agrawal@hashedin.com>
2021-01-05 23:12:10 +0530

    added static repo json data, fetch issues from github added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import HomePage from "./components/HomePage";
-import { userState, userEffect } from 'react';
 import './App.css';
 
 function App() {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React from "react";
+import Data from "../repodata.json";
 
 const handleSearch = (e) => {
-  fetch(`https://api.github.com/repos/${e.target.value}`)
-  .then(res => res.json())
-  .then(data => {
-    setData(data)
-  })
-}
+  // const completeUri = `https://api.github.com/repos/${e.target.value}`;
+  // fetch(completeUri)
+  // .then(res => res.json())
+  // .then(data => console.log(data))
+  // .finally(issueList(completeUri))
 
-const setData = ({})
+  Data.Data.forEach((data) => {
+    if (data.full_name === e.target.value) {
+      console.log("found it", data.full_name);
+      issueList(data.full_name)
+      return data.full_name;
+    }
+  });
+};
 
-const SearchBar = ({keyword,setKeyword}) => {
-  const SearchBarStyling = {width:"13rem",background:"#F2F1F9", border:"none", padding:"0.5rem", marginTop  : "50px"};
+const issueList = (value) => {
+  fetch(`https://api.github.com/repos/${value}/issues`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+
+const SearchBar = ({ keyword, setKeyword }) => {
+  const SearchBarStyling = {
+    width: "13rem",
+    background: "#F2F1F9",
+    border: "none",
+    padding: "0.5rem",
+    marginTop: "50px",
+  };
   return (
-    <input 
-     style={SearchBarStyling}
-     key="key"
-     value={keyword}
-     placeholder={"Type Github Repository ex. React"}
-     onChange={handleSearch}
+    <input
+      style={SearchBarStyling}
+      key="key"
+      value={keyword}
+      placeholder={"Type Github Repository ex. React"}
+      onChange={handleSearch}
     />
   );
-}
+};
 
-export default SearchBar
+export default SearchBar;
