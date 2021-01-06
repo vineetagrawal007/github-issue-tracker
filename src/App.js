fd329d66e5b083ea7c99620b3d583ed9725b1dd3
Vineet Kumar Agrawal <vineet.agrawal@hashedin.com>
2021-01-06 11:01:31 +0530

    refactor and add search capability

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import HomePage from "./components/HomePage";
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <HomePage/>
-  );
+// function App() {
+//   return (
+//     <HomePage/>
+//   );
+// }
+
+// export default App;
+
+export default class App extends React.Component {
+  render()
+  {
+    return (
+      <HomePage/>
+    )
+  }
 }
-
-export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Data from "../repodata.json";
+import "../App.css";
 
-const handleSearch = (e) => {
-  // const completeUri = `https://api.github.com/repos/${e.target.value}`;
-  // fetch(completeUri)
-  // .then(res => res.json())
-  // .then(data => console.log(data))
-  // .finally(issueList(completeUri))
+//example repo full name: mojombo/grit
 
-  Data.Data.forEach((data) => {
-    if (data.full_name === e.target.value) {
-      console.log("found it", data.full_name);
-      issueList(data.full_name)
-      return data.full_name;
-    }
-  });
-};
+export default function SearchBar() {
+  //state
+  const [query, setQuery] = useState([]);
 
-const issueList = (value) => {
-  fetch(`https://api.github.com/repos/${value}/issues`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-};
+  const handleSearch = (e) => {
+    Data.Data.forEach((data) => {
+      if (data.full_name === e.target.value) {
+        console.log("found it, Repo Name: ", data.full_name);
+        setQuery(data.full_name);
+        issueList(data.full_name);
+        return data.full_name;
+      }
+    });
+  };
 
-const SearchBar = ({ keyword, setKeyword }) => {
+  const issueList = (value) => {
+    fetch(`https://api.github.com/repos/${value}/issues`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   const SearchBarStyling = {
     width: "13rem",
     background: "#F2F1F9",
@@ -31,15 +32,65 @@ const SearchBar = ({ keyword, setKeyword }) => {
     padding: "0.5rem",
     marginTop: "50px",
   };
-  return (
-    <input
-      style={SearchBarStyling}
-      key="key"
-      value={keyword}
-      placeholder={"Type Github Repository ex. React"}
-      onChange={handleSearch}
-    />
-  );
-};
 
-export default SearchBar;
+  return (
+    <>
+    <form>
+      <input
+        style={SearchBarStyling}
+        key="key"
+        placeholder={"Type Github Repository ex. React"}
+        onChange={handleSearch}
+      />
+      
+    </form>
+
+    <div className='card-list'>
+      {query}
+      </div>
+    </>
+  );
+}
+
+// const handleSearch = (e) => {
+//   // const completeUri = `https://api.github.com/repos/${e.target.value}`;
+//   // fetch(completeUri)
+//   // .then(res => res.json())
+//   // .then(data => console.log(data))
+//   // .finally(issueList(completeUri))
+
+//   Data.Data.forEach((data) => {
+//     if (data.full_name === e.target.value) {
+//       console.log("found it, Repo Name: ", data.full_name);
+//       issueList(data.full_name)
+//       return data.full_name;
+//     }
+//   });
+// };
+
+// const issueList = (value) => {
+//   fetch(`https://api.github.com/repos/${value}/issues`)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// };
+
+// const SearchBar = ({ keyword, setKeyword }) => {
+//   const SearchBarStyling = {
+//     width: "13rem",
+//     background: "#F2F1F9",
+//     border: "none",
+//     padding: "0.5rem",
+//     marginTop: "50px",
+//   };
+//   return (
+//     <input
+//       style={SearchBarStyling}
+//       key="key"
+//       value={keyword}
+//       placeholder={"Type Github Repository ex. React"}
+//       onChange={handleSearch}
+//     />
+//   );
+// };
+
+// export default SearchBar;
